6728f37a5e3d6149c92b226eabff045f1ce40710
Javier <javidx@gmail.com>
2023-01-09 19:22:24 +0200

    Added Book data storage on localStorage HTML element loading and appending to index.html event listener to add button

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 let books = [];
+let storedBooks = JSON.parse(localStorage.getItem('local-books'));
+const addButton = document.getElementById('add');
+const newBookTitle = document.getElementById('title');
+const newBookAuthor = document.getElementById('author');
+const bookList = document.getElementById('book-list');
 
+if (storedBooks) {
+  books = storedBooks;
+  books.forEach(book => {
+    bookElement(book)
+  });
+}
 
-function newBook(title, author) {
+function booksStored() {
+  books = JSON.parse(localStorage.getItem('local-books'));
+}
+
+function bookElement(bookData) {
+  const bookItem = document.createElement('li');
+  bookItem.id = `${bookData.id}`
+  const bookItemTitle = document.createElement('h2')
+  bookItemTitle.textContent = bookData.title
+  const bookItemAuthor = document.createElement('h3')
+  bookItemAuthor.textContent = bookData.author
+  const bookRemoveButton = document.createElement('button')
+  bookRemoveButton.textContent = 'Remove';
+  bookRemoveButton.id = `button-${bookData.id}`;
+  bookItem.append(bookItemTitle, bookItemAuthor, bookRemoveButton)
+  bookList.appendChild(bookItem);
+}
+
+function newBookItem() {
+  const book = addBook(newBookTitle.value, newBookAuthor.value);
+  bookElement(book);
+}
+
+function newBook(title, author, id) {
   this.title = title;
   this.author = author;
+  this.id = id;
 }
 
 // adding books
 function addBook(title, author) {
-  books.push(new newBook(title, author));
+  const book = new newBook(title, author, books.length)
+  books.push(book);
+  return book;
 }
 
 // Removing books
@@ -16,13 +53,8 @@ function removeBook() {
   
 }
 
-
-
-// Testing 
-
-addBook('Other book', 'Jane Jones');
-addBook('Other book', 'Jane Jones');
-addBook('Other book', 'Jane Jones');
-addBook('Other book', 'Jane Jones');
-console.log(books);
-
+addButton.addEventListener(('click'), (e) => {
+  e.preventDefault()
+  newBookItem()
+  localStorage.setItem('local-books',JSON.stringify(books));
+})
