60191e67eadcf9fadda1c8668201155ef1086e07
Javier <javidx@gmail.com>
2023-01-11 14:02:04 +0200

    Added: navbar dynamic contact form Modified: Behavior of contact form classes

--- app.js
+++ app.js
@@ -106,19 +106,23 @@ addButton.addEventListener(('click'), ((e) => {
 
 const bookStored = document.querySelector('.books-stored')
 const booksAdd = document.querySelector('.books-add')
+const contactForm = document.querySelector('.contact-form')
 
 document.querySelector('.nav').addEventListener('click',((e)=>{
   if (e.target.innerText === 'List'){
     bookStored.classList.add('books-stored-appear') 
     booksAdd.classList.add('books-add-disappear')
+    contactForm.classList.add('contact-form-disappear')
     console.log(bookStored);
   }
   if (e.target.innerText === 'Add new'){
     bookStored.classList.remove('books-stored-appear')
     booksAdd.classList.remove('books-add-disappear')
-
+    contactForm.classList.add('contact-form-disappear')
   }
   if (e.target.innerText === 'Contact'){
-    
+    bookStored.classList.remove('books-stored-appear')
+    booksAdd.classList.add('books-add-disappear')
+    contactForm.classList.remove('contact-form-disappear')
   }
 }))
